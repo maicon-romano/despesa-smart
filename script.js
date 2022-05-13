acd6c2324eeed9d1d360299e41270a95c2cce88b
Maicon Romano <shardersfx@gmail.com>
2022-05-13 16:52:37 -0300

    Transaction, amount JS

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const dummyTransactions = [
   { id: 3, name: 'Torta de frango', amount: -10}
   { id: 4, name: 'Violão', amount: 150}
 ]
+
+const addTransactionIntoDOM = transaction => {
+  const operator = transaction.amount < 0 ? '-' : '+'
+  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+  const amountWithoutOperator = Math.abs(transaction.amount)
+  const li = document.createElement('li')
+
+  li.classList.add(CSSClass)
+  li.innerHTML = `
+  ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+  `
+  console.log(li)
+}
+
+addTransactionIntoDOM(dummyTransactions[1])
