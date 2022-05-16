560baf0fc254365da61e74aec088ae8d3b1517e3
Maicon Romano <shardersfx@gmail.com>
2022-05-16 18:25:54 -0300

    update balance values

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
+const transactionsUl = document.querySelector("#transactions");
+
 const dummyTransactions = [
-  { id: 1, name: 'Bolo de brigadeiro', amount: -20},
-  { id: 2, name: 'Salário', amount: 300},
-  { id: 3, name: 'Torta de frango', amount: -10}
-  { id: 4, name: 'Violão', amount: 150}
-]
+  { id: 1, name: "Bolo de brigadeiro", amount: -20 },
+  { id: 2, name: "Salário", amount: 300 },
+  { id: 3, name: "Torta de frango", amount: -10 },
+  { id: 4, name: "Violão", amount: 150 },
+];
 
 const addTransactionIntoDOM = transaction => {
-  const operator = transaction.amount < 0 ? '-' : '+'
-  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
-  const amountWithoutOperator = Math.abs(transaction.amount)
-  const li = document.createElement('li')
+  const operator = transaction.amount < 0 ? "-" : "+";
+  const CSSClass = transaction.amount < 0 ? "minus" : "plus";
+  const amountWithoutOperator = Math.abs(transaction.amount);
+  const li = document.createElement("li");
 
-  li.classList.add(CSSClass)
+  li.classList.add(CSSClass);
   li.innerHTML = `
-  ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
-  `
-  console.log(li)
-}
+  ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">`;
+  transactionsUl.append(li);
+};
 
-addTransactionIntoDOM(dummyTransactions[1])
+const updateBalanceValues = () => {
+  const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount);
+  const total = transactionsAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
+  const income = transactionsAmounts.filter()
+  console.log(total);
+};
+
+const init = () => {
+  dummyTransactions.forEach(addTransactionIntoDOM);
+  updateBalanceValues();
+};
+
+init();
