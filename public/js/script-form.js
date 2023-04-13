420f88bf7b07bf94223fc48c1a500debff9164a6
Maicon Romano <shardersfx@gmail.com>
2023-04-13 00:19:45 -0300

    Nova tela de login

--- public/js/script-form.js
+++ public/js/script-form.js
@@ -1,12 +1,10 @@
-document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault(); // Impede que o formulário seja enviado
-
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+document.querySelector(".btn-login").addEventListener("click", function () {
+  var username = document.querySelector('input[name="usuario"]').value;
+  var password = document.querySelector('input[name="senha"]').value;
 
   if (username === "MGdespesas" && password === "Controle2023") {
     // Se o nome de usuário e a senha estiverem corretos, redirecionar para a página de escolha de controle
-    window.location.href = "/pages/escolha-controle.html";
+    window.location.href = "/public/pages/escolha-controle.html";
     alert("Bem vindo!");
   } else {
     // Se o nome de usuário e a senha estiverem incorretos, mostrar uma mensagem de erro
