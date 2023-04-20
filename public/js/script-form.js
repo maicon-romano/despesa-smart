e0cfc23ccf2e517bb35584ea056fbb19e51b517b
Maicon Romano <shardersfx@gmail.com>
2023-04-19 23:32:17 -0300

    API autenticação firebase, credenciais do usuário

--- public/js/script-form.js
+++ public/js/script-form.js
@@ -1,49 +1,56 @@
 document.querySelector(".btn-login").addEventListener("click", function () {
-  var username = document.querySelector('input[name="usuario"]').value;
+  var email = document.querySelector('input[name="email"]').value;
   var password = document.querySelector('input[name="senha"]').value;
 
-  if (username === "MGdespesas" && password === "Controle2023") {
-    // Se o nome de usuário e a senha estiverem corretos, redirecionar para a página de escolha de controle
-    window.location.href = "./pages/escolha-controle.html";
-    alert("Bem vindo!");
-  } else {
-    // Se o nome de usuário e a senha estiverem incorretos, mostrar uma mensagem de erro
-    alert("Nome de usuário ou senha incorretos. Tente novamente.");
-  }
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Se as credenciais estiverem corretas, redirecionar para a página de escolha de controle
+      window.location.href = "./pages/escolha-controle.html";
+      alert("Bem vindo!");
+    })
+    .catch((error) => {
+      // Se as credenciais estiverem incorretas, mostrar uma mensagem de erro
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
 });
 
 function registrar() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  auth.createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // User registered successfully
-    var user = userCredential.user;
-    console.log(user);
-  })
-  .catch((error) => {
-    // Registration failed
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage);
-  });
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // User registered successfully
+      var user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      // Registration failed
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
 }
 
 function entrar() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
-  auth.signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // User logged in successfully
-    var user = userCredential.user;
-    console.log(user);
-  })
-  .catch((error) => {
-    // Login failed
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage);
-  });
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // User logged in successfully
+      var user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      // Login failed
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
 }
