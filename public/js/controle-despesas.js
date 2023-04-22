9a249d0ead66b36b0d0a6cdb1bf7b1b6c0643068
Maicon Romano <shardersfx@gmail.com>
2023-04-22 00:00:55 -0300

    Informações de receita e despesa dinâmica através do banco de dados do firebase, adicionando e removendo, de forma dinâmica pelo usuário logado

--- public/js/controle-despesas.js
+++ public/js/controle-despesas.js
@@ -1,39 +1,121 @@
-const pageName = window.location.pathname.split("/").pop().replace(".html", "");
+var firebaseConfig = {
+  apiKey: "AIzaSyDrquZN4aqqAAZr2g60mFuUSkTHyOI84Eg",
+  authDomain: "despesasmart.firebaseapp.com",
+  projectId: "despesasmart",
+  storageBucket: "despesasmart.appspot.com",
+  messagingSenderId: "348685765695",
+  appId: "1:348685765695:web:4913207469c96328a13c8a",
+  measurementId: "G-CB8JDXKMDX",
+};
+window.firebase.initializeApp(firebaseConfig);
 
-const transactionsUl = document.querySelector("#transactions");
-const incomeDisplay = document.querySelector("#money-plus");
-const expenseDisplay = document.querySelector("#money-minus");
-const balanceDisplay = document.querySelector("#balance");
-const form = document.querySelector("#form");
-const inputTransactionName = document.querySelector("#text");
-const inputTransactionAmount = document.querySelector("#amount");
+const auth = window.firebase.auth();
+const db = window.firebase.firestore();
+let currentUser;
+let transactions = [];
 
-const getStoredTransactions = () => {
-  const localStorageTransactions = localStorage.getItem(
-    `${pageName}-transactions`
-  );
-  return localStorageTransactions ? JSON.parse(localStorageTransactions) : [];
+const transactionsUl = document.getElementById("transactions");
+const incomeDisplay = document.getElementById("money-plus");
+const expenseDisplay = document.getElementById("money-minus");
+const balanceDisplay = document.getElementById("balance");
+const form = document.getElementById("form");
+const inputTransactionName = document.getElementById("text");
+const inputTransactionAmount = document.getElementById("amount");
+
+const getTransactionsFromDB = async () => {
+  transactions = []; // Limpa o array de transações antes de buscar no banco de dados
+
+  const userId = currentUser.uid;
+  const userTransactionsRef = db
+    .collection("transactions")
+    .doc(userId)
+    .collection("userTransactions");
+
+  try {
+    const querySnapshot = await userTransactionsRef.get();
+    querySnapshot.forEach((doc) => {
+      const transaction = {
+        id: doc.id,
+        description: doc.data().description,
+        value: doc.data().value,
+      };
+      transactions.push(transaction);
+    });
+
+    // Adicionar transações pendentes
+    const pendingTransactions = transactions.filter((t) => !t.id);
+    for (const pendingTransaction of pendingTransactions) {
+      const docRef = await userTransactionsRef.add({
+        description: pendingTransaction.description,
+        value: pendingTransaction.value,
+      });
+      pendingTransaction.id = docRef.id;
+    }
+
+    init();
+  } catch (error) {
+    console.error("Erro ao recuperar transações do usuário:", error);
+  }
 };
 
-let transactions = getStoredTransactions();
+const addTransactionToDB = async (transaction) => {
+  const userId = currentUser.uid;
+  const userTransactionsRef = db
+    .collection("transactions")
+    .doc(userId)
+    .collection("userTransactions");
 
-const removeTransaction = (ID) => {
-  transactions = transactions.filter((transaction) => transaction.id !== ID);
-  updateLocalStorage();
+  try {
+    const docRef = await userTransactionsRef.add({
+      description: transaction.description,
+      value: transaction.value,
+    });
+    console.log("Transação adicionada ao banco de dados");
+    transaction.id = docRef.id;
+    init();
+  } catch (error) {
+    console.error("Erro ao adicionar transação ao banco de dados:", error);
+  }
+};
+
+const removeTransactionFromDB = (transactionId) => {
+  const userId = currentUser.uid;
+  const userTransactionsRef = db
+    .collection("transactions")
+    .doc(userId)
+    .collection("userTransactions")
+    .doc(transactionId);
+
+  userTransactionsRef
+    .delete()
+    .then(() => {
+      console.log("Transação removida do banco de dados");
+    })
+    .catch((error) => {
+      console.error("Erro ao remover transação do banco de dados:", error);
+    });
+};
+
+const removeTransaction = (transactionId) => {
+  transactions = transactions.filter(
+    (transaction) => transaction.id !== transactionId
+  );
+  removeTransactionFromDB(transactionId);
   init();
 };
 
-const addTransactionIntoDOM = ({ amount, name, id }) => {
-  const operator = amount < 0 ? "-" : "+";
-  const CSSClass = amount < 0 ? "minus" : "plus";
-  const amountWithoutOperator = Math.abs(amount).toFixed(2);
+const addTransactionIntoDOM = ({ id, description, value }) => {
+  const operator = value < 0 ? "-" : "+";
+  const CSSClass = value < 0 ? "minus" : "plus";
+  const valueWithoutOperator = Math.abs(value).toFixed(2);
   const li = document.createElement("li");
 
   li.classList.add(CSSClass);
+  li.setAttribute("data-id", id);
   li.innerHTML = `
-    ${name}
-    <span>${operator} R$ ${amountWithoutOperator}</span>
-    <button class="delete-btn" onClick="removeTransaction(${id})">x</button>
+    ${description}
+    <span>${operator} R$ ${valueWithoutOperator}</span>
+    <button class="delete-btn">x</button>
   `;
   transactionsUl.append(li);
 };
@@ -57,7 +139,7 @@ const getTotal = (transactionsAmounts) =>
     .toFixed(2);
 
 const updateBalanceValues = () => {
-  const transactionsAmounts = transactions.map(({ amount }) => amount);
+  const transactionsAmounts = transactions.map(({ value }) => value);
   balanceDisplay.textContent = `R$ ${getTotal(transactionsAmounts)}`;
   incomeDisplay.textContent = `R$ ${getIncome(transactionsAmounts)}`;
   expenseDisplay.textContent = `R$ ${getExpenses(transactionsAmounts)}`;
@@ -69,211 +151,133 @@ const init = () => {
   updateBalanceValues();
 };
 
-init();
+const updateTransactionsInDB = () => {
+  const userId = currentUser.uid;
+  const userTransactionsRef = db
+    .collection("transactions")
+    .doc(userId)
+    .collection("userTransactions");
 
-const updateLocalStorage = () => {
-  localStorage.setItem(
-    `${pageName}-transactions`,
-    JSON.stringify(transactions)
-  );
+  userTransactionsRef
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const transaction = transactions.find((t) => t.id === doc.id);
+        if (!transaction) {
+          userTransactionsRef
+            .doc(doc.id)
+            .delete()
+            .then(() => {
+              console.log("Transação removida do banco de dados");
+            })
+            .catch((error) => {
+              console.error(
+                "Erro ao remover transação do banco de dados:",
+                error
+              );
+            });
+        }
+      });
+      transactions.forEach((transaction) => {
+        if (!transaction.id) {
+          userTransactionsRef
+            .add({
+              description: transaction.description,
+              value: transaction.value,
+            })
+            .then(() => {
+              console.log("Transação adicionada ao banco de dados");
+            })
+            .catch((error) => {
+              console.error(
+                "Erro ao adicionar transação ao banco de dados:",
+                error
+              );
+            });
+        } else {
+          userTransactionsRef
+            .doc(transaction.id)
+            .update({
+              description: transaction.description,
+              value: transaction.value,
+            })
+            .then(() => {
+              console.log("Transação atualizada no banco de dados");
+            })
+            .catch((error) => {
+              console.error(
+                "Erro ao atualizar transação no banco de dados:",
+                error
+              );
+            });
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao recuperar transações do usuário:", error);
+    });
 };
 
-const generateID = () => Math.round(Math.random() * 1000);
-
-const addToTransactionsArray = (transactionName, transactionAmount) => {
-  transactions.push({
-    id: generateID(),
-    name: transactionName,
-    amount: parseFloat(transactionAmount),
-  });
-};
-
-const cleanInputs = () => {
-  inputTransactionName.value = "";
-  inputTransactionAmount.value = "";
-};
-
-const isValidInput = (transactionName, transactionAmount) => {
-  if (transactionName === "" || transactionAmount === "") {
-    alert("Por favor, preencha tanto o nome quanto o valor da transação");
-    return false;
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    currentUser = user;
+    console.log("Usuário autenticado:", user.uid);
+    getTransactionsFromDB();
+  } else {
+    console.log("Usuário não autenticado");
+    transactionsUl.innerHTML = "";
   }
+});
 
-  if (
-    isNaN(parseFloat(transactionAmount.replace(",", "."))) ||
-    parseFloat(transactionAmount.replace(",", ".")) === 0
-  ) {
-    alert("Por favor, digite um valor válido para a transação");
-    return false;
-  }
-
-  return true;
-};
-
-const handleFormSubmit = (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const transactionName = inputTransactionName.value.trim();
-  const transactionAmount = inputTransactionAmount.value
-    .trim()
-    .replace(",", ".");
+  const description = inputTransactionName.value.trim();
+  const value = parseFloat(inputTransactionAmount.value);
 
-  if (!isValidInput(transactionName, transactionAmount)) {
+  if (!description || isNaN(value)) {
+    console.error(
+      "Os campos de entrada devem ser preenchidos e o valor deve ser um número válido"
+    );
     return;
   }
 
-  addToTransactionsArray(transactionName, transactionAmount);
-  init();
-  updateLocalStorage();
-  cleanInputs();
-};
+  const transaction = {
+    id: "",
+    description: description,
+    value: value,
+  };
+  transactions.push(transaction);
+  addTransactionToDB(transaction); // Chame addTransactionToDB aqui
+  addTransactionIntoDOM(transaction);
+  updateBalanceValues();
+  // getTransactionsFromDB(); // Chame getTransactionsFromDB aqui
+  inputTransactionName.value = "";
+  inputTransactionAmount.value = "";
+});
 
-form.addEventListener("submit", handleFormSubmit);
-
-window.onload = function () {
-  const auth = firebase.auth();
-  const transactionsCollectionRef = firebase
-    .firestore()
-    .collection("transactions");
-
-  function addTransaction(description, value) {
-    const user = firebase.auth().currentUser;
-
-    if (user) {
-      transactionsCollectionRef
-        .doc(user.uid)
-        .collection("userTransactions")
-        .add({
-          description: description,
-          value: value,
-        })
-        .then(() => {
-          console.log("Transação adicionada com sucesso!");
-          getUserTransactions();
-        })
-        .catch((error) => {
-          console.error("Erro ao adicionar transação:", error);
-        });
-    } else {
-      console.error("Usuário não autenticado");
-    }
+transactionsUl.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    const li = event.target.closest("li");
+    const transactionId = li.getAttribute("data-id");
+    removeTransaction(transactionId);
   }
+});
 
-  function removeTransaction(transactionId) {
-    const user = firebase.auth().currentUser;
-
-    if (user) {
-      transactionsCollectionRef
-        .doc(user.uid)
-        .collection("userTransactions")
-        .doc(transactionId)
-        .delete()
-        .then(() => {
-          console.log("Transação removida com sucesso!");
-          getUserTransactions();
-        })
-        .catch((error) => {
-          console.error("Erro ao remover transação:", error);
-        });
-    } else {
-      console.error("Usuário não autenticado");
-    }
-  }
-
-  function getUserTransactions() {
-    const user = firebase.auth().currentUser;
-
-    if (user) {
-      transactionsCollectionRef
-        .doc(user.uid)
-        .collection("userTransactions")
-        .get()
-        .then((querySnapshot) => {
-          const transactionsList = document.getElementById("transactions");
-          transactionsList.innerHTML = "";
-
-          querySnapshot.forEach((doc) => {
-            console.log(doc.id, " => ", doc.data());
-
-            const transactionElement = document.createElement("li");
-            transactionElement.textContent = `${doc.data().description} - R$ ${
-              doc.data().value
-            }`;
-
-            const removeButton = document.createElement("button");
-            removeButton.textContent = "X";
-            removeButton.onclick = () => {
-              removeTransaction(doc.id);
-            };
-            transactionElement.appendChild(removeButton);
-
-            transactionsList.appendChild(transactionElement);
-          });
-        })
-        .catch((error) => {
-          console.error("Erro ao recuperar transações do usuário:", error);
-        });
-    } else {
-      console.error("Usuário não autenticado");
-    }
-  }
-
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      console.log("Usuário autenticado:", user.uid);
-      getUserTransactions();
-    } else {
-      console.log("Usuário não autenticado");
-      const transactionsList = document.getElementById("transactions");
-      transactionsList.innerHTML = "";
-    }
-  });
-
-  const form = document.getElementById("form");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const description = document.getElementById("text").value;
-    const value = parseFloat(document.getElementById("amount").value);
-
-    if (!description || isNaN(value)) {
-      console.error(
-        "Os campos de entrada devem ser preenchidos e o valor deve ser um número válido"
-      );
-      return;
-    }
-
-    addTransaction(description, value);
-
-    document.getElementById("text").value = "";
-    document.getElementById("amount").value = "";
-  });
-
-  const logoutButton = document.getElementById("logout-button");
-  logoutButton.addEventListener("click", () => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        console.log("Usuário desconectado");
-      })
-      .catch((error) => {
-        console.error("Erro ao desconectar usuário:", error);
-      });
-  });
-
-  const loginButton = document.getElementById("login-button");
-  loginButton.addEventListener("click", () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-        console.log("Usuário autenticado com sucesso:", result.user.uid);
-        getUserTransactions();
-      })
-      .catch((error) => {
-        console.error("Erro ao autenticar usuário:", error);
-      });
-  });
-};
+const logoutButton = document.getElementById("logout-button");
+logoutButton.addEventListener("click", () => {
+  auth
+    .signOut()
+    .then(() => {
+      console.log("Usuário desconectado");
+      currentUser = null;
+      transactions = [];
+      transactionsUl.innerHTML = "";
+      balanceDisplay.textContent = "R$ 0.00";
+      incomeDisplay.textContent = "R$ 0.00";
+      expenseDisplay.textContent = "R$ 0.00";
+    })
+    .catch((error) => {
+      console.error("Erro ao desconectar usuário:", error);
+    });
+});
